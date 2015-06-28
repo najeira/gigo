211f0856dbf950cfaf51372d5243a7743bcf4cf5
najeira <najeira@gmail.com>
2015-06-28 21:25:09 +0900

    add test for tail

--- plugins/in_tail/in_tail_test.go
+++ plugins/in_tail/in_tail_test.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 	"strings"
 	"sync"
 	"testing"
+	"time"
 )
 
 type testLogger struct {
@@ -19,27 +22,27 @@ type testLogger struct {
 }
 
 func (l *testLogger) Debugf(format string, args ...interface{}) {
-	l.debug.WriteString(fmt.Sprintf(format, args...))
+	l.debug.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 func (l *testLogger) Infof(format string, args ...interface{}) {
-	l.info.WriteString(fmt.Sprintf(format, args...))
+	l.info.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 func (l *testLogger) Noticef(format string, args ...interface{}) {
-	l.notice.WriteString(fmt.Sprintf(format, args...))
+	l.notice.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 func (l *testLogger) Warnf(format string, args ...interface{}) {
-	l.warn.WriteString(fmt.Sprintf(format, args...))
+	l.warn.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 func (l *testLogger) Errorf(format string, args ...interface{}) {
-	l.err.WriteString(fmt.Sprintf(format, args...))
+	l.err.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 func (l *testLogger) Criticalf(format string, args ...interface{}) {
-	l.crit.WriteString(fmt.Sprintf(format, args...))
+	l.crit.WriteString(fmt.Sprintf(format, args...) + "\n")
 }
 
 type testEmitter struct {
@@ -49,8 +52,12 @@ type testEmitter struct {
 func (e *testEmitter) Emit(msg interface{}) (err error) {
 	if b, ok := msg.([]byte); ok {
 		_, err = e.buffer.Write(b)
+		e.buffer.WriteString("\n")
+		//fmt.Println(string(b))
 	} else if s, ok := msg.(string); ok {
 		_, err = e.buffer.WriteString(s)
+		e.buffer.WriteString("\n")
+		//fmt.Println(s)
 	} else {
 		err = fmt.Errorf("unknown type")
 	}
@@ -140,7 +147,7 @@ func TestHandleErrPipe(t *testing.T) {
 	wg.Wait()
 
 	rets := l.warn.String()
-	if rets != "thisistest" {
+	if rets != "this\nis\ntest\n" {
 		t.Errorf("invalid warn")
 	}
 }
@@ -154,7 +161,7 @@ func TestHandleLine(t *testing.T) {
 	p.handleLine("test")
 
 	rets := e.buffer.String()
-	if rets != "thisistest" {
+	if rets != "this\nis\ntest\n" {
 		t.Errorf("invalid emit")
 	}
 }
@@ -181,7 +188,7 @@ func TestHandleOutPipe(t *testing.T) {
 	wg.Wait()
 
 	rets := e.buffer.String()
-	if rets != "thisistest" {
+	if rets != "this\nis\ntest\n" {
 		t.Errorf("invalid emit")
 	}
 }
@@ -215,12 +222,76 @@ func TestHandlePipes(t *testing.T) {
 	wg.Wait()
 
 	rets := e.buffer.String()
-	if rets != "thisistest" {
+	if rets != "this\nis\ntest\n" {
 		t.Errorf("invalid emit")
 	}
 
 	rets2 := l.warn.String()
-	if rets2 != "thisistest" {
+	if rets2 != "this\nis\ntest\n" {
 		t.Errorf("invalid warn")
 	}
+}
+
+func TestTail(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	path := f.Name()
+
+	defer func() {
+		f.Close()
+		os.Remove(path)
+	}()
+
+	e := testEmitter{}
+	l := testLogger{}
+	p := New(Config{Emitter: &e, Logger: &l, File: path})
+
+	err = p.Start()
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+
+	_, err = f.WriteString("this\n")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = f.WriteString("is\n")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = f.WriteString("test\n")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = f.Sync()
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+
+	err = p.Stop()
+	if err != nil {
+		t.Error(err)
+	}
+
+	rets := e.buffer.String()
+	if rets != "this\nis\ntest\n" {
+		t.Errorf("invalid emit: %s", rets)
+	}
+
+	rets2 := l.warn.String()
+	if rets2 != "" {
+		t.Errorf("invalid warn: %s", rets2)
+	}
+
+	//fmt.Println(l.debug.String())
 }
