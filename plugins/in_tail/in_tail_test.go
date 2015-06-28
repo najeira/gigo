58b32a936045b6e06d85160017b5a2113d692e4c
najeira <najeira@gmail.com>
2015-06-28 17:37:05 +0900

    add test of scan

--- plugins/in_tail/in_tail_test.go
+++ plugins/in_tail/in_tail_test.go
@@ -1,6 +1,10 @@
 package in_tail
 
 import (
+	"bytes"
+	"io"
+	"strings"
+	"sync"
 	"testing"
 )
 
@@ -19,5 +23,57 @@ func TestTrimCrLf(t *testing.T) {
 	}
 	if trimCrLf("hoge\n\r") != "hoge" {
 		t.Fail()
+	}
+}
+
+type scanReceiver struct {
+	buffer bytes.Buffer
+	err    error
+}
+
+func (s *scanReceiver) handle(line string) {
+	_, s.err = s.buffer.WriteString(line)
+}
+
+func TestScan(t *testing.T) {
+	p := New(Config{})
+
+	var ret bytes.Buffer
+	r, w := io.Pipe()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		p.scan(r, func(line string) {
+			ret.WriteString(line)
+			ret.WriteString("\n")
+		})
+		wg.Done()
+	}()
+
+	w.Write([]byte("this\n"))
+	w.Write([]byte("is\n"))
+	w.Write([]byte("test\n"))
+	w.Close()
+
+	wg.Wait()
+
+	lines := strings.Split(ret.String(), "\n")
+
+	if len(lines) != 4 {
+		t.Errorf("invalid lines")
+	}
+	if lines[0] != "this" {
+		t.Errorf("invalid line")
+	}
+	if lines[1] != "is" {
+		t.Errorf("invalid line")
+	}
+	if lines[2] != "test" {
+		t.Errorf("invalid line")
+	}
+	if lines[3] != "" {
+		t.Errorf("invalid line")
 	}
 }
