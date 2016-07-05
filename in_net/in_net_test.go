a8729a4975276884465c7886ec15d8f196734e42
najeira <najeira@gmail.com>
2016-07-05 16:33:54 +0900

    update in_net test

--- in_net/in_net_test.go
+++ in_net/in_net_test.go
@@ -103,4 +103,110 @@ func TestReader(t *testing.T) {
 	if warns := l.Warn.String(); warns != "" {
 		t.Errorf("invalid warn: %s", warns)
 	}
+
+	checkNoWarnNoError(t, l)
+}
+
+func TestReader2(t *testing.T) {
+	addr := ":9753"
+
+	l := testutil.Logger{}
+	p, err := Open(Config{Logger: &l, Net: "tcp", Addr: addr})
+	if err != nil {
+		t.Fatal(err)
+	}
+	scanner := bufio.NewScanner(p)
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = conn.Write([]byte("this\n"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if scanner.Scan() {
+		line := scanner.Text()
+		if line != "this" {
+			t.Errorf("expect 'this' got: '%s'", line)
+		}
+	} else {
+		t.Errorf("scan failed")
+	}
+
+	_, err = conn.Write([]byte("is\n"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if scanner.Scan() {
+		line := scanner.Text()
+		if line != "is" {
+			t.Errorf("expect 'is' got: '%s'", line)
+		}
+	} else {
+		t.Errorf("scan failed")
+	}
+
+	_, err = conn.Write([]byte("test\n"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if scanner.Scan() {
+		line := scanner.Text()
+		if line != "test" {
+			t.Errorf("expect 'test' got: '%s'", line)
+		}
+	} else {
+		t.Errorf("scan failed")
+	}
+
+	if err := conn.Close(); err != nil {
+		t.Error(err)
+	}
+
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = conn.Write([]byte("second\n"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if scanner.Scan() {
+		line := scanner.Text()
+		if line != "second" {
+			t.Errorf("expect 'second' got: '%s'", line)
+		}
+	} else {
+		t.Errorf("scan failed")
+	}
+
+	if err := conn.Close(); err != nil {
+		t.Error(err)
+	}
+
+	if err := scanner.Err(); err != nil {
+		t.Error(err)
+	}
+
+	if err = p.Close(); err != nil {
+		t.Error(err)
+	}
+
+	checkNoWarnNoError(t, l)
+}
+
+func checkNoWarnNoError(t *testing.T, l testutil.Logger) {
+	if warns := l.Warn.String(); warns != "" {
+		t.Error(warns)
+	}
+	if errs := l.Error.String(); errs != "" {
+		t.Error(errs)
+	}
 }
